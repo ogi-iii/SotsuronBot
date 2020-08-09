a6818398dcbc8ed1f5061ea1c9f611134beff085
*TlSunGQf66 <kazuki@hacarus.com>
2020-08-09 10:59:38 +0900

    docx added

--- slack_notify.py
+++ slack_notify.py
@@ -5,7 +5,7 @@ slackURL="https://hooks.slack.com/services/TCQANTS3F/BFW6H8VQ8/JyKkdBB6R5eoGZer6
 
 def main():
     payload={
-        "text":"New file uploaded!\nhttps://github.com/",
+        "text":"New file uploaded! \nfile: https://github.com/ogi-iii/Sotsuron_CI_CD/ \ntextlint: https://travis-ci.org/github/ogi-iii/Sotsuron_CI_CD",
         "username":"SotsuronBot",
         "icon_emoji":":cubimal_chick:"
     }
