c8a09784c6e4885fb8750162237a9c5e25b69b35
*TlSunGQf66 <kazuki@hacarus.com>
2020-08-09 11:21:17 +0900

    ci modified

--- slack_notify.py
+++ slack_notify.py
@@ -1,15 +1,18 @@
 import json
 import requests
+import sys
 
 slackURL="https://hooks.slack.com/services/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"
 
 def main():
-    payload={
-        "text":"New file uploaded! \nfile: https://github.com/ogi-iii/Sotsuron_CI_CD/ \ntextlint: https://travis-ci.org/github/ogi-iii/Sotsuron_CI_CD",
-        "username":"SotsuronBot",
-        "icon_emoji":":cubimal_chick:"
+    text = f"New file uploaded! \nfile download: https://github.com/ogi-iii/Sotsuron_CI_CD/raw/ogi_sotsuron/ogi_sotsuron.docx \ntextlint: {sys.argv[1]}"
+    payload = {
+        "text": text,
+        "username": "SotsuronBot",
+        "icon_emoji": ":cubimal_chick:",
+        "color": "#00FFFF",
     }
-    data=json.dumps(payload)
+    data = json.dumps(payload)
     requests.post(slackURL, data)
 
 if __name__ == "__main__":
